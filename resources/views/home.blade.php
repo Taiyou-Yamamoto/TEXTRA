20b695faac7b14506d002fe3a70b71feca7ad4f1
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-09-03 20:38:54 +0900

    書籍追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,33 @@
 
 @section('title', 'Home')
 
-@section('content_header')
+{{-- @section('content_header')
 
-@stop
+@stop --}}
 
 @section('content')
-    <div class=" p-4 rounded-md grid grid-cols-5 ">
-        <form action="{{ url('/book/register') }}" method="get"
-            class="items-center flex flex-col  justify-center place-items-center border-4 border-dashed  border-blue-700 h-72 cursor-pointer hover:text-indigo-400 hover:border-indigo-400 transition-all duration-100">
+
+
+    <div class=" p-4 grid grid-cols-5 ">
+        <a href="{{ route('book.register') }}" method="GET"
+            class="items-center flex flex-col rounded-md justify-center place-items-center border-4 border-dashed  border-blue-700 h-72 cursor-pointer hover:text-indigo-400 hover:border-indigo-400 transition-all duration-100">
             <i class="fas fa-plus text-3xl mr-4 text-blue-600 mx-auto transition-all duration-100"></i>
             <p class="text-shadow text-blue-600 text-2xl  hover:text-indigo-400 mx-auto transition-all duration-100">本を追加する
             </p>
-        </form>
+        </a>
 
-        {{-- @foreach
-        @endforeach --}}
+        @foreach ($books as $book)
+            <a href="{{ route('book.register') }}" method="GET">
+                <div href="{{ route('book.register') }}" method="GET"
+                    class="items-center rounded-md place-items-center border-4 ml-6 w-52 h-64">
+                    <img src="{{ asset('img/D4123631-D9E9-44CE-B6BC-CD30034EABCC_1_201_a.jpeg') }}" alt="img"
+                        class=" rounded-md">
+                </div>
+                <p>{{ $book->title }}</p>
+            </a>
+        @endforeach
     </div>
+    <button></button>
 @stop
 
 @section('css')
@@ -26,7 +37,5 @@
 @stop
 
 @section('js')
-    <script>
-        console.log('Hi!');
-    </script>
+    <script></script>
 @stop
