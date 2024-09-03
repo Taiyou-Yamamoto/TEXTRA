0d37a79b55e260abdf3f6e13456ef6df732231de
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-09-03 22:16:02 +0900

    home画面　完成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,37 +2,42 @@
 
 @section('title', 'Home')
 
-{{-- @section('content_header')
-
-@stop --}}
-
 @section('content')
 
+    <div class="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 h-auto">
+        <!-- 本を追加するボタン -->
+        <div>
+            <a href="{{ route('book.register') }}" method="GET"
+                class="flex flex-col items-center justify-center rounded-md border-4 border-dashed h-72 w-full
+                border-blue-600 hover:border-indigo-600 transition-all duration-200 hover:bg-blue-300">
+                <i class="fas fa-plus text-4xl text-blue-600 hover:text-indigo-600 transition-all duration-200"></i>
 
-    <div class=" p-4 grid grid-cols-5 ">
-        <a href="{{ route('book.register') }}" method="GET"
-            class="items-center flex flex-col rounded-md justify-center place-items-center border-4 border-dashed  border-blue-700 h-72 cursor-pointer hover:text-indigo-400 hover:border-indigo-400 transition-all duration-100">
-            <i class="fas fa-plus text-3xl mr-4 text-blue-600 mx-auto transition-all duration-100"></i>
-            <p class="text-shadow text-blue-600 text-2xl  hover:text-indigo-400 mx-auto transition-all duration-100">本を追加する
+            </a>
+            <p
+                class="text-blue-600 font-semibold text-2xl mt-1 text-center hover:text-indigo-700 transition-all duration-200">
+                本を追加する
             </p>
-        </a>
+        </div>
 
+
+        <!-- 本のカード -->
         @foreach ($books as $book)
-            <a href="{{ route('book.register') }}" method="GET">
-                <div href="{{ route('book.register') }}" method="GET"
-                    class="items-center rounded-md place-items-center border-4 ml-6 w-52 h-64">
+            <a href="{{ route('book.register') }}" method="GET" class="hover:text-blue-600 transition-all duration-200">
+                <div
+                    class="rounded-md bg-white shadow-md overflow-hidden h-72 w-full hover:shadow-xl transition-shadow duration-200">
                     <img src="{{ asset('img/D4123631-D9E9-44CE-B6BC-CD30034EABCC_1_201_a.jpeg') }}" alt="img"
-                        class=" rounded-md">
+                        class="w-full object-cover">
                 </div>
-                <p>{{ $book->title }}</p>
+                <p class="text-2xl font-semibold mt-1 text-center overflow-hidden text-ellipsis whitespace-nowrap">
+                    {{ $book->title }}
+                </p>
             </a>
         @endforeach
     </div>
-    <button></button>
+
 @stop
 
 @section('css')
-    {{-- <link rel="stylesheet" href="{{ mix('css/home.css') }}"> --}}
     @vite(['resources/css/home.css'])
 @stop
 
