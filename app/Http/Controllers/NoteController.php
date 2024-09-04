60294a082b43d2bd10e69858b7fab6b93dc7100e
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-09-04 14:13:53 +0900

    メモ登録画面　

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Note;
+
 use Illuminate\Support\Facades\Auth;
 
 class NoteController extends Controller
@@ -11,5 +13,20 @@ class NoteController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        // 商品一覧取得
+        $notes = Note::all();
+
+        return view('note.index', compact('notes'));
+    }
+
+    public function register()
+    {
+
+
+        return view('note.register');
     }
 }
---
+++ resources/views/note/register.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'Home')
+
+@section('content')
+
+    <h1>hello</h1>
+
+@stop
+
+@section('css')
+    @vite(['resources/css/home.css'])
+@stop
+
+@section('js')
+    <script></script>
+@stop
