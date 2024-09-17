52ddf94ec5c3f54293e684d0cd7dd1186dd750ba
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-09-17 11:59:32 +0900

    エラーメッセージの表記を修正

--- resources/views/book/register.blade.php
+++ resources/views/book/register.blade.php
@@ -3,59 +3,58 @@
 @section('title', '商品登録')
 
 @section('content_header')
-    <h1 class='text-center pt-12'>新しい本を追加</h1>
+    <h1 class='text-center pt-12 font-semibold text-2xl white_shadow'>新しい本を追加</h1>
 @stop
 
 @section('content')
     <div class="row">
         <div class="col-md-10 mx-auto">
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
 
-            <div class="card card-primary h-90">
+            <div class="card card-primary h-90 w-8/12 mx-auto">
                 <form action="{{ route('book.add') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="card-body flex h-90">
                         <div class="w-80 pt-3 mt-6 rounded">
                             {{-- イメージ --}}
-                            <img id="displayImage" name="img_path" src="{{ asset('storage/img/bookimage.jpg') }}" alt=""
-                                class="aspect-3/5 h-4/6 w-3/5 mx-auto mb-4 rounded-md">
+                            <img id="displayImage" name="img_path" src="{{ asset('storage/img/bookimage.jpg') }}"
+                                alt="" class="aspect-3/5 h-4/6 w-3/5 mx-auto mb-4 rounded-md">
 
                             {{-- アップロード --}}
-                            <div class="flex flex-col items-start space-y-4">
-                                <label for="fileUpload"
-                                    class="text-sm font-semibold text-gray-400">表紙を変更する場合は画像を選んでください</label>
-                                <input type="file" id="fileUpload" name="image_path" accept="image/*,image/svg+xml"
-                                    class="block w-full text-sm text-gray-500 file:border file:border-gray-300 my-0 file:py-2 file:px-4 file:rounded-md file:text-sm file:font-medium file:bg-gray-100 hover:file:bg-gray-200">
+                            <div class="flex flex-col items-start space-y-4 ml-6">
+                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    for="file_input">表紙を変更する場合は画像を選んでください</label>
+                                <input
+                                    class="block w-full text-sm file:p-2 file:font-semibold file:cursor-pointer text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                    id="file_input" type="file">
                             </div>
 
                         </div>
-
-                        <div class="flex-grow-1 mx-4 my-10">
-                            <div class="form-group my-5">
-                                <label for="title">タイトル</label>
+                        {{-- タイトル --}}
+                        <div class="flex-grow-1 mx-4 mt-5">
+                            <div class="form-group mt-5 mb-3">
+                                <label for="title gray_shadow">タイトル</label>
                                 <input type="text" class="form-control " id="title" name="title"
-                                    placeholder="本のタイトル">
+                                    placeholder="本のタイトル" value="{{ old('title')}}">
                             </div>
+                            @if ($errors->has('title'))
+                                <li class="list-none text-red-600">{{ $errors->first('title') }}</li>
+                            @endif
 
+                            {{-- 種別 --}}
                             <div class="form-group mt-6">
-                                <label for="type">種別</label>
-                                <input type="text" class="form-control" id="type" name="type" placeholder="種別">
+                                <label for="type gray_shadow">種別</label>
+                                <input type="text" class="form-control" id="種別" name="type" placeholder="種別" value="{{ old('type')}}">
                             </div>
+                            @if ($errors->has('type'))
+                                <li class="list-none text-red-600">{{ $errors->first('type') }}</li>
+                            @endif
 
                         </div>
 
                     </div>
 
                     <div class="card-footer text-center">
-                        <button type="submit" class="btn btn-primary">本を登録</button>
+                        <button type="submit" class="btn btn-primary shadow hover:shadow-none">本を登録</button>
                     </div>
                 </form>
             </div>
