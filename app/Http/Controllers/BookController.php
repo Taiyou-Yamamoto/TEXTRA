5de9a4c51e3496969b674bc27309e78ecd8588ab
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-09-22 15:42:12 +0900

    修正

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -57,7 +57,7 @@ class BookController extends Controller
         // } else {
         //     $validated['image_path'] = 'img/bookimage.jpg';
         // }
-        if($request['image_path'] == 'img/bookimage.jpg'){
+        if ($request['image_path'] == 'img/bookimage.jpg') {
             $validated['image_path'] = 'img/bookimage.jpg';
         }
 
