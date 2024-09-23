8371f9576378b835d19c81356fa269a63960dc1d
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-09-23 18:31:26 +0900

    dd

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -60,6 +60,7 @@ class BookController extends Controller
         if (!$request->hasFile('image_path')) {
             $validated['image_path'] = 'img/bookimage.jpg';
         } else if($request->hasFile('image_path')){
+            dd(config('filesystems.disks.s3.region'));
             $file = $request->file('image_path');
             $validated['image_path'] = $file->hashName();
             Storage::disk('s3')->put('covers', $validated['image_path']);
